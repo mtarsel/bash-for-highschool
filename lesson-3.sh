80306b084f44ef1fd236c9c6dec13048ed59bef8
Mick Tarsel <mtarsel@gmail.com>
2022-02-13 20:23:25 -0800

    more comments and cleaner ouput
    Signed-off-by: Mick Tarsel <mtarsel@gmail.com>

--- lesson-3.sh
+++ lesson-3.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-#example of reading variables and subtraction in bash
+#try running this command outside this script in a new terminal window
+# $> date +"%Y"
+#  2022
+# $> date 
+#  Sun 13 Feb 2022 08:13:29 PM PST
+#
+#
+#
+#this script is an example of using variables and doing
+#   subtraction with numbers in bash
 
 echo What year were you born?
 read birth_year
@@ -8,10 +17,12 @@ read birth_year
 #output from command is saved with $(some command )
 current_year=$(date +"%Y")
 
-echo The current year is:
-echo $current_year
+# DEBUG:  print out the variable we defined to make sure it is correct
+#echo The current year is:
+#echo $current_year
 
-echo By end of this year, you will be...
+# subtract the 2 variables that were defined
+echo By end of $current_year, you will be...
 echo $(expr $current_year - $birth_year)
 
 echo Enter future year:

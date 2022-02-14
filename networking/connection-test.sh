eccdda8050c6c39f48559e1847f0100f3d05d8b3
Mick Tarsel <mtarsel@gmail.com>
2022-02-13 20:39:29 -0800

    re-organized networking dir.
    server.sh should be executed where we want to serve up files
    
    Signed-off-by: Mick Tarsel <mtarsel@gmail.com>

--- networking/connection-test.sh
+++ networking/connection-test.sh
@@ -2,7 +2,7 @@
 
 echo Is the network up?
 
-if ping -c 1 8.8.8.8 &> /dev/null; then
+if ping -c1 8.8.8.8 &> /dev/null; then
   echo network is up
 else
   echo network is down
@@ -12,7 +12,7 @@ fi
 #run a loop while pinging google
 echo trying looop
 while ! ping -c 1 -W 1 8.8.8.8; do
-    echo "Waiting for 1.2.3.4 - network interface might be down..."
+    echo "Waiting for 8.8.8.8 - network interface might be down..."
     sleep 1
 done
 
---
+++ networking/server/start-server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# open a web browser using the correct ip address and port
+python3 -m http.server
+

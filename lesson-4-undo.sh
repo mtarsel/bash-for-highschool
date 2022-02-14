e33510c7cce8d0583567db510249ecfcb913ede4
Mick Tarsel <mtarsel@gmail.com>
2022-02-13 20:09:25 -0800

    lesson-4 renmaed and undo script included
    Signed-off-by: Mick Tarsel <mtarsel@gmail.com>

---
+++ lesson-4-undo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#undo everything lesson-4 did
+
+#remove the directory and all files in that directory
+rm -rf ./images
+
+#TODO what if this script is executed in a different directory?

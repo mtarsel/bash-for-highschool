ffd27fc1c548e5fce076cf5758a09a825353f85b
Mick Tarsel <mtarsel@gmail.com>
2022-02-13 20:05:00 -0800

    for loop and current working directory
    use a for loop to look at the type of every file.
    
    download some image and move it to a dir.
    
    Signed-off-by: Mick Tarsel <mtarsel@gmail.com>

--- lesson-4-images.sh
+++ lesson-4-images.sh
@@ -1,8 +1,48 @@
 #!/bin/bash
 
-#make all gifs smaller with convert
-for f in *gif; do convert "$f" -resize 200x200 smaller-"$f"; done
+#this script will demonstrate the importance of the current working directory
+# as well as show an exmaple of a for loop
 
-#move all pictures to images directory
-for type in png gif jpg; do mv *.$type images; done
+#the directory where a bash script is executed is very important
+#just like running a bash command in the wrong directory could 
+#   change a different file than you intended to change
+#
+#
+#
+# Find and print out any file with the extenstion jpeg in the current directory
+# ls -a | grep *.jpeg
+#
+#
+# if there are no jpeg file in the current working directory
+#   there will be nothing to print out and the script will continue to run
 
+
+#makes a new directory for images inside bash-for-highschool dir
+mkdir images
+
+#download some image
+wget https://i.imgur.com/FrsUgiH.jpeg
+
+#for every type of file in this directory that is a jpeg, 
+# echo (print out) a statement that says "found .jpeg!"
+for type in jpeg; do echo found .jpeg!; done
+
+echo sleeping for 3 seconds...
+#let the terminal sleep for 3 seconds
+sleep 3
+echo im awake!
+
+#print out the current working directory
+pwd
+
+echo moving jpeg into ./images...
+#move every .jpeg
+mv *.jpeg ./images
+
+
+ls -a | grep *.jpeg
+
+echo end of the script!
+
+
+#TODO what are some variables to add to this script in order to reduce repeat words?

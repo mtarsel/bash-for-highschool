c11b2c616a67c23169c911cf0b5f5624f7ff1d02
Mick Tarsel <mtarsel@gmail.com>
2023-10-26 22:00:35 -0700

    add challenge and fix formatting
    challenge is to create a backup of all the shell scripts in the current working directory and move them to a directory with a day month year timestamp

--- lesson-4.sh
+++ lesson-4.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
-#for every type of file in this directory that is a jpeg, 
-# echo (print out) a statement that says "found .jpeg!"
+#for every type of file in this directory that is a shell script
 for some_file in *.sh; do
     echo "========="
     echo "Found file named: $some_file"
     echo "========="
-    $(cat $some_file)
+    #cat command prints the contents of the file
+    #print the content of file using echo
+    echo "$(cat $some_file)"
     echo ""
 done
+# Challenge: Make a backup with a timestamp
+# move all bash script lessons to a backup directory. 
+#  the directory should include the month, day, and year
+# hint: mv is the move command that moves files. 
+# hint: cp is the copy command to copy files
+# hint: date command knows the month day year
 
+
+
+backup_dir = "/home/backup-2023/$(date +"%m-%d-%Y")/"
+mkdir -p $backup_dir
+
+for bash_script in *.sh; do
+    mv $bash_script $backup_dir
+    echo "moved $bash_script to $backup_dir"
+done
+echo "backup complete!"
 
 
 #TODO
